6bfd7998feb010ebcae7f03aeaf0c2ee4375a835
yngong <yngong0519@gmail.com>
2025-09-30 14:17:12 +0800

    fix run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 python -m vida.rename_fasta_header -i BVBRC_genome_sequence.fasta -o renamed.fas -m renamed.map
 mafft renamed.fas > aligned.fsa
-iqtree -s aligned.fsa -B 1000 -alrt 1000
+iqtree -s aligned.fsa -B 1000 -alrt 1000 -redo
 # manually modify map file
-python -m vida.restore_fasta_header -i aligned.fsa.contree -o aligned_restored.fsa.contree -m renamed.map
+python -m vida.restore_fasta_header -i aligned.fsa.contree -o aligned_restored.fsa.contree -m renamed_modified.map
