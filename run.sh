5d6855cfc86e35ee5b3f72d02bab12436ba53d7f
yngong <yngong0519@gmail.com>
2025-09-30 12:22:47 +0800

    first commit

---
+++ run.sh
@@ -0,0 +1,5 @@
+python -m vida.rename_fasta_header -i BVBRC_genome_sequence.fasta -o renamed.fas -m renamed.map
+mafft renamed.fas > aligned.fsa
+iqtree -s aligned.fsa -B 1000 -alrt 1000
+# manually modify map file
+python -m vida.restore_fasta_header -i aligned.fsa.contree -o aligned_restored.fsa.contree -m renamed.map
